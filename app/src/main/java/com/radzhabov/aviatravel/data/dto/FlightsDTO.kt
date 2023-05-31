5d57cee53d3c2c41fc68c92d9e491471b5b85de4
Ikrom <nacud85@gmail.com>
2023-05-31 18:12:21 +0300

    fix dto file

--- app/src/main/java/com/radzhabov/aviatravel/data/dto/FlightsDTO.kt
+++ app/src/main/java/com/radzhabov/aviatravel/data/dto/FlightsDTO.kt
@@ -1,14 +1,5 @@
 package com.radzhabov.aviatravel.data.dto
 
-//class FlightsDTO : ArrayList<FlightDTO>()
-//
-//data class FlightDTO(
-//    val airport: String,
-//    val city: String,
-//    val code: String,
-//    val country: String
-//)
-
 data class FlightsDTO(
     val flights: List<FlightDTO>
 )
